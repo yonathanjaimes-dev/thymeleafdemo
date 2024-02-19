1440c5c7567d113c7f8e9a7c7a087fe6d0392848
Yonathan <yonathanjaimes97@duck.com>
2024-02-19 15:58:48 -0500

    added sout message

--- src/main/java/com/learning/thymeleafdemo/controller/StudentController.java
+++ src/main/java/com/learning/thymeleafdemo/controller/StudentController.java
@@ -21,6 +21,7 @@ public class StudentController {
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student") Student theStudent){
         System.out.println("theStudent: "+theStudent.getFirstName()+" "+theStudent.getLastName());
+        System.out.println("This is the only student");
         return "student-confirmation";
     }
 }
