88a3bbdcdec8ed9092d686e39165e29337cba8a9
Yonathan <yonathanjaimes97@duck.com>
2024-02-20 11:25:39 -0500

    Version 2 added radio, checkbox, dropdown options to the form

--- src/main/java/com/learning/thymeleafdemo/model/Student.java
+++ src/main/java/com/learning/thymeleafdemo/model/Student.java
@@ -1,26 +1,49 @@
 package com.learning.thymeleafdemo.model;
 
+import java.util.List;
+
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteOS;
+
+
 
     public Student() {
 
     }
-
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteOS() {
+        return favoriteOS;
+    }
+
+    public void setFavoriteOS(List<String> favoriteOS) {
+        this.favoriteOS = favoriteOS;
     }
 }
